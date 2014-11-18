09e27b585b4d427805a0950d11c0ace58b9c3137
Estela <estelapedrero@gmail.com>
2014-11-18 16:11:41 +0100

    dia2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
 	def index
+		if user_signed_in?
+   		render 'user'
+   	else 
+   	  render 'login'
+		end
 	end
 
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ActiveRecord::Base
 	has_many :books
+	belongs_to :user
 end
