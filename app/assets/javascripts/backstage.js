9f505d96e05218f092680f44d66c322e007bcef2
Estela <estelapedrero@gmail.com>
2014-11-19 12:33:39 +0100

    change backstage

---
+++ app/assets/javascripts/backstage.js
@@ -0,0 +1,13 @@
+var stage = document.getElementsByClassName('main-stage');
+var stages = document.getElementsByClassName('stages')
+
+for (i=0; i<stages.length; i++){
+
+stages[i].onclick = changeBackStage;
+
+};
+
+function changeBackStage(e){
+	console.log(e.target)
+	stage[0].style.backgroundImage = "url("+ e.target.src+ ")";
+}
--- app/assets/javascripts/draganddrop.js
+++ app/assets/javascripts/draganddrop.js
@@ -2,9 +2,6 @@ $(function() {
     $(".draggable").draggable({ containment: "#stage" });
   });
 
-
-
-
 $('.sortable-list').sortable({
 		connectWith: '.sortable-list'
 	});
