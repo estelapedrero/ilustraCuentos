bf8f4ad73db71ea75259b8cfea77cf17aedfba30
Estela <estelapedrero@gmail.com>
2014-11-20 11:35:36 +0100

    remove dibujos

--- app/assets/javascripts/draganddrop.js
+++ app/assets/javascripts/draganddrop.js
@@ -5,10 +5,7 @@ $(function() {
     	drag: function(event, ui){
       		$("#posx").text(ui.position.left);
       		$("#posy").text(ui.position.top);
-  		},
-		stop: function( event, ui ) {
-		   $('.delete').addClass('.show-button');
-		}
+  		}
     });
   });
 
