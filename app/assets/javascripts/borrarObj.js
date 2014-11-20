f03b41da2bcdc7725c176c251171160488a101fa
Estela <estelapedrero@gmail.com>
2014-11-20 14:35:57 +0100

    homepage y ruta a books

--- app/assets/javascripts/borrarObj.js
+++ app/assets/javascripts/borrarObj.js
@@ -3,11 +3,10 @@ var toolsbox = document.getElementsByClassName('tools-box');
 var dibujo = document.getElementsByClassName('tools');
 
 
-for (i=0; i<equis.length; i++){
+for (var i=0; i<equis.length; i++){
 	equis[i].addEventListener('click',deleteObj.bind(this,i));
 };
 
 function deleteObj(i){
 	equis[i].parentNode.setAttribute('style', 'position:relative;');
 };
-
