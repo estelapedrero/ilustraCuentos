4c445a12f7514e493c257d41d6fb7509fd28bb7f
Estela <estelapedrero@gmail.com>
2014-11-23 20:21:04 +0100

    centrar contenido y pequeños arreglos

--- app/assets/javascripts/draganddrop.js
+++ app/assets/javascripts/draganddrop.js
@@ -2,33 +2,29 @@ $(function() {
 	var equis = ""
 
     $(".draggable").draggable({
-    	helper: function(){
-    		return $(this).clone().appendTo('.main-stage').css({
-                'zIndex': 5
-            });
-    	},
     	cursor: 'move',
-        containment: ".main-stage"
+        containment: "#stage",
+        helper: function(){
+    		return $(this).clone().appendTo('.main-stage');
+    	}
     });
 
-    $('.main-stage').droppable({
-    	activeClass: 'ui-state-hover',
+    $('#stage').droppable({
     	accept: '.draggable',
     	activate: function(ev,ui){
     		equis = document.getElementsByClassName('delete-button');
-
     		for (var i=0; i<equis.length; i++){
 				equis[i].addEventListener('click',deleteObj.bind(this,i));
 			};
     	},
 		drop: function(ev, ui) {
-		//alert(ev.clientX);
-			console.log($(document.createElement('p')).text(ui.offset.left));
-			console.log($(document.createElement('p')).text(ui.offset.top));
-			if (!ui.draggable.hasClass(".draggable"))
-                $(this).append($(ui.draggable).clone().addClass(".draggable").draggable());
-            }
-	});
+			console.log(ui.offset.left);
+			console.log(ui.offset.top);
+            if (!ui.draggable.hasClass("draggable"))
+                var dropped = $(ui.draggable).clone().addClass("draggable").draggable();
+                dropped.css({'left': ui.position.left, 'top': ui.position.top});         
+	   }
+    });
 
 	function deleteObj(i){
 		equis[i].parentNode.remove();
