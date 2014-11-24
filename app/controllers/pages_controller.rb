38ac564f508e4f214158701755411f1404a7924d
Estela <estelapedrero@gmail.com>
2014-11-24 19:10:15 +0100

    se guardan los textos/pag

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,23 @@ class PagesController < ApplicationController
 	def create
 		@book = Book.find(params[:book_id])
 		@page = @book.pages.create page_params
-		redirect_to books_path
+		if params["commit"] == "+"
+			redirect_to edit_book_page_path(@book, @page)
+
+		else
+			redirect_to books_path
+
+		end
+	end
+
+	def edit
+			@book = Book.find(params[:book_id])
+			@page = @book.pages.find(params[:id])
+			render 'books/edit'
+	end
+	def update
+		@book = Book.find(params[:book_id])
+		redirect_to books_path  
 	end
 
 		private	
