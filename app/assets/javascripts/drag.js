676189dc79d1b611816ce09fb6a8d74ae90b52d7
Estela <estelapedrero@gmail.com>
2014-11-25 16:16:02 +0100

    alert json objetos

--- app/assets/javascripts/drag.js
+++ app/assets/javascripts/drag.js
@@ -6,11 +6,35 @@
         }
     }
     var addStageElement = function () {
-        $('#stage').append("<div class='stage-element'><img src='"+this.dataset.element+"'><i class='delete-button fa fa-times'></i></div>");
-         console.log(this)
+        $('#stage').append("<div class='stage-element' data-src='"+this.dataset.element+"'><i class='delete-button fa fa-times'></i><img src='"+this.dataset.element+"'></div>");
         $( ".stage-element" ).draggable({ containment: "parent", snap: false});
       }
+
+
+    var getJson = function() {
+      var stageElements = $('.stage-element').map( function( ) {
+          $top = $(this).css("top");
+          $left = $(this).css("left");
+          $img = $(this).data("src");
+
+          $background = document.getElementsByClassName('stage-element')[0].parentNode.style.backgroundImage;
+
+        return {top: $top, left: $left, img: $img, background: $background};
+
+      }).get();
+      alert(JSON.stringify(stageElements));
+
+    }
+
+
+
+
+
+
+
     $( document ).ready(function() {
       $( ".tools" ).on( "click", addStageElement );
       $( "#stage" ).on( "click", removeStageElement );
+      $( "#btnjson" ).on( "click", getJson );
+
     });
